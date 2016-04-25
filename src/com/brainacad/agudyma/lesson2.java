311eac966379f54982eb7875a92714273b64bacd
AGudyma <crowley14@gmail.com>
2016-04-25 19:35:59 +0300

    initial commit

---
+++ src/com/brainacad/agudyma/lesson2.java
@@ -0,0 +1,21 @@
+package com.brainacad.agudyma;
+import java.util.Scanner;
+/**
+ * Created by User on 25.04.2016.
+ */
+public class lesson2 {
+            public static void main(String[] args) {
+            System.out.println("Please enter your Name");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            System.out.println("Please enter your date of birth");
+            String dob = scanner.nextLine();
+            System.out.println(name+" "+dob);
+        }
+    }
+
+
+
+
+
+
